61a46841c248e03b84b2bbbcfc634a7d75b51056
Abdulai059 <abdulaiosman8080@gmail.com>
2025-08-30 11:56:26 +0000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import Header from "./component.jsx/Header";
 import Main from "./component.jsx/Main";
 
 function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Error"));
+  }, []);
+
   return (
     <div className="app">
       <Header />
