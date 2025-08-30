cd8b5e6e0753378decad258e3f082153bb894f8a
Abdulai059 <abdulaiosman8080@gmail.com>
2025-08-30 12:45:17 +0000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./component.jsx/Header";
 import Main from "./component.jsx/Main";
 
+// Initial state
+const initialization = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialization);
+
+  // Reducer
+  function reducer(state, action) {
+    switch (action.type) {
+      case "dataReceived":
+        return {
+          ...state,
+          questions: action.payload,
+          status: "ready",
+        };
+      case "data Failed":
+        return {
+          ...state,
+          status: "error",
+        };
+
+      default:
+        throw new Error("Action unknown");
+    }
+  }
+
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error("Error"));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFiled" }));
   }, []);
 
   return (
